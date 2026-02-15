07be5d3d420919084196a2a94f7c13fddc478fb1
bdartt <76175950+bdartt@users.noreply.github.com>
2026-02-15 11:49:22 -0600

    Change to not inject scripts in prod config

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -8,7 +8,7 @@ module.exports = merge(common, {
   plugins: [
     new HtmlWebpackPlugin({
       template: './index.html',
-      scriptLoading: 'blocking',
+      inject: false,
     }),
     new CopyPlugin({
       patterns: [
